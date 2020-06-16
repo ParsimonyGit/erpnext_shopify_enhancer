3455b42fa837eba27b07c627f83f6c931c709b86
ashish-greycube <mr.ashish.shah@gmail.com>
2020-06-16 10:43:20 +0530

    donot check..just cut

--- erpnext_shopify_enhancer/item.py
+++ erpnext_shopify_enhancer/item.py
@@ -1,12 +1,18 @@
 import frappe
-
+import json
 @frappe.whitelist(allow_guest=True)
 def truncate_item_name_from_shopify(self,name):
-    frappe.msgprint('Inside truncate_item_name_from_shopify',self.name)
+    # print(frappe._dict(self))
+    print('---'*100,self.item_name)
+    frappe.msgprint('Inside truncate_item_name_from_shopify',self.item_name)
     enable_shopify=frappe.db.get_single_value('Shopify Settings', 'enable_shopify')
+    print('enable_shopify',enable_shopify)
     frappe.msgprint('enable_shopify',enable_shopify)
     if enable_shopify==1:
-        if self.sync_with_shopify==1:
-            frappe.msgprint('sync_with_shopify',self.sync_with_shopify,self.item_name,len(self.item_name))
-            if len(self.item_name)>140:
-                self.item_name=self.item_name[0:140]
+        print('1'*100,self.item_name)
+        frappe.msgprint('sync_with_shopify',self.item_name)
+        self.item_name=self.item_name[0:140]
+        frappe.msgprint('after sync_with_shopify',self.item_name)
+        print('2'*100,self.item_name)
+        # return self
+
